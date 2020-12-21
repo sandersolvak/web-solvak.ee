a3d6ffa4c0c7f0e9eb32eef09dffb020c4737701
sandersolvak <sander.solvak@funderbeam.com>
2020-12-21 13:46:50 +0200

    Updated main nav to show active item

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -33,13 +33,14 @@
 
 <body>
 	<nav>
-		<a href="<?= $site->url() ?>" class="home active"><span class="hidden">Home</span>
+		<a href="<?= $site->url() ?>" class="home"><span class="hidden">Home</span>
 			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 40">
 			  <path d="M27.1 14.2c5.3 0 8.5 5.3 8.9 10.3l-.7.1c-.2 0-.3-.1-.4-.2-1.5-4-5.9-4.7-9.6-3.3-2.2.8-6.3 3.3-8.5 4C9.9 27.7 4.5 22.4 4 15.5l.7-.1c.2 0 .3.1.4.2 1.5 4 5.9 4.7 9.6 3.3 3.8-1.4 8.1-4.8 12.4-4.7z"/>
 			</svg>
 		</a>
-		<?php
-		foreach ($site->children()->listed() as $item): ?>
-		<?= $item->title()->link() ?>
+
+		<?php foreach ($site->children()->listed() as $item): ?>
+			<a<?php e($item->isOpen(), ' class="active"') ?> href="<?= $item->url() ?>"><?= $item->title()->html() ?></a>
 		<?php endforeach ?>
+
 	</nav>
