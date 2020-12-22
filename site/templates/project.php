a159a8d68f47870c620281894d55e0e1b7e69b1d
sandersolvak <sander.solvak@funderbeam.com>
2020-12-22 22:28:44 +0200

    Simplified project template and fixed up content

--- site/templates/project.php
+++ site/templates/project.php
@@ -16,45 +16,55 @@
 		</header>
 
 		<div class="col-2">
-			<?= $page->intro() ?>
+			<?= $page->intro()->kt() ?>
 		</div>
 
-		<!-- text in left column -->
+		<section class="wide">
+			<?= $page->text()->kt() ?>
+		</section>
+
+		<!-- text in left column
 		<div class="col-1">
 			<p>Designing the tool's UI posed some interesting challenges such as how does one visualise certain agile practices such as sprint cadence and velocity characteristics and the patterns that emerge.</p>
-		</div>
+		</div>-->
 
-		<!-- text in right column -->
+		<!-- text in right column
 		<div class="col-2">
 			<p>We made our best effort to practice what we preach and work as an agile team, effectively using the tool on ourselves while building it.</p>
-		</div>
+		</div>-->
 
-		<!-- regular image -->
+		<!-- regular image
 		<img class="wide" src="playtech-002.jpg" alt="" loading=lazy>
-
-		<!-- image w. caption -->
+-->
+		<!-- image w. caption
 		<figure class="wide">
 			<img src="logos-JointVentureProjects.jpg" alt="" loading=lazy>
 			<figcaption>Joint Venture Projects</figcaption>
 		</figure>
-
-		<!-- video -->
+-->
+		<!-- video
 		<figure class="wide">
 			<video  preload="none" poster="statlab-web-poster.jpg" autoplay playsinline loop muted>
 				<source src="statlab-web.webm" type="video/webm" >
 				<source src="statlab-web.mp4" type="video/mp4" >
 			</video>
 		</figure>
-
-		<section>
-			<?= $page->text()->kt() ?>
-		</section>
-
-		<a href="../work.html" class="link-left">See all projects</a>
+-->
 
 	</article>
 
-	<a href="<?= url('work') ?>">View all projects</a>
+	<div class="pagenav">
+		<a href="<?= url('work') ?>">All projects</a>
+		<?php if ($page->hasPrevListed()): ?>
+			<a href="<?= $page->prevListed()->url() ?>" class="link-left">previous</a>
+			<?php else: ?>
+			<span class="link-left">previous</span>
+		<?php endif ?>
+		<?php if ($page->hasNextListed()): ?>
+			<a href="<?= $page->nextListed()->url() ?>" class="link-right">next</a>
+		<?php else: ?><span class="link-right">next</span>
+		<?php endif ?>
+	</div>
 
 </main>
 
