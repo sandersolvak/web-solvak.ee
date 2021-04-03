faf2c1135c8b4f70a43c49b846abbe303a576a5d
sandersolvak <sander.solvak@funderbeam.com>
2021-04-03 20:28:23 +0300

    Shows the title of the previous and next post in previous-next navigation

--- site/snippets/pagenav.php
+++ site/snippets/pagenav.php
@@ -6,12 +6,12 @@
 
  <div class="pagenav">
 	 <?php if ($page->hasPrevListed()): ?>
-		 <a href="<?= $page->prevListed()->url() ?>" class="link-left">Older</a>
+		 <a href="<?= $page->prevListed()->url() ?>" class="link-left" title="Previous: <?= $page->prevListed()->title() ?>"><?= $page->prevListed()->title()->short(40) ?></a>
 		 <?php else: ?>
 		 <span>← Older</span>
 	 <?php endif ?>
 	 <?php if ($page->hasNextListed()): ?>
-		 <a href="<?= $page->nextListed()->url() ?>" class="link-right">Newer</a>
+		 <a href="<?= $page->nextListed()->url() ?>" class="link-right" title="Next: <?= $page->nextListed()->title() ?>"><?= $page->nextListed()->title()->short(40) ?></a>
 	 <?php else: ?><span>Newer →</span>
 	 <?php endif ?>
  </div>
